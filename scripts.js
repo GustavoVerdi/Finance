ed1321c1fc4bec1edb554b3ab5b9cdfb59b0c520
Gustavo Verdi <140762138+GustavoVerdi@users.noreply.github.com>
2024-03-12 10:39:58 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -40,3 +40,59 @@ const myChart = new Chart(ctx, {
     scales: { y: { beginAtZero: true } }
   }
 });
+function toggleForm(formId) {
+  var form = document.getElementById('adicionar-' + formId);
+  form.style.display = form.style.display === 'none' ? 'block' : 'none';
+}
+var button = document.querySelector(".b1");
+var select = document.querySelector(".b2");
+button.onclick = function () {
+  select.style.display = "block";
+}
+var overlay = document.getElementById("overlay");
+var modal = document.getElementById("modal");
+var open = document.querySelector(".b1");
+var close = document.getElementById("close");
+var ok = document.getElementById("ok");
+
+open.onclick = function () {
+  overlay.style.display = "block";
+}
+
+close.onclick = function () {
+  overlay.style.display = "none";
+}
+
+ok.onclick = function () {
+  overlay.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == overlay) {
+      overlay.style.display = "none";
+  }
+}
+var alterar = document.querySelector("#alt");
+var excluir = document.querySelector("#exc");
+
+alterar.addEventListener("click", function () {
+  // Escreva aqui o código que você quer que seja executado quando o botão de alterar for clicado
+  alert("Você clicou no botão de alterar");
+});
+
+excluir.addEventListener("click", function () {
+  // Escreva aqui o código que você quer que seja executado quando o botão de excluir for clicado
+  alert("Você clicou no botão de excluir");
+});
+// Seleciona todos os elementos com a classe b1
+var botoes = document.getElementsByClassName("b1");
+// Percorre cada elemento
+for (var i = 0; i < botoes.length; i++) {
+  // Adiciona um listener de evento de clique
+  botoes[i].addEventListener("click", function () {
+      // Escreva aqui o código que você quer que seja executado quando o botão for clicado
+      alert("Você clicou no botão " + this.innerHTML);
+  });
+}
+if (document.querySelector('body').offsetHeight > window.innerHeight)
+document.querySelector('footer').classList.add('footer-relative');
